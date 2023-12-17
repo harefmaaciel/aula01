f29895baed86836ee70d25a9beefd952a9b1b3a0
harefmaaciel <harefvmaciel@gmail.com>
2023-12-17 17:03:55 -0300

    primeiro commit

---
+++ aula04.js
@@ -0,0 +1,13 @@
+function calculaIMC (peso, altura) {
+    return peso / (altura * altura)
+    
+}
+
+
+const peso = 80
+const altura = 1.73
+const resultadoIMC = calculaIMC(peso, altura)
+console.log(resultadoIMC)
+
+
+
